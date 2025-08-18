f1709b300a2b7fc9e42f820970c0c84d531eeed6
Guilherme Nastari <atacamapresentes@gmail.com>
2025-08-18 13:34:23 -0300

    Funcao Farenheit funcional

--- js/arquivo.js
+++ js/arquivo.js
@@ -14,6 +14,18 @@ function ConversaoCelsiusParaFarenheit() {
         let FuncaoFarenheit = ( entradaCelsiusNumero * ( 9 / 5 ) ) + 32;
         let FuncaoFarenheitNumber = Number(FuncaoFarenheit);
         let resultadoF = document.getElementById('resultadoFarenheit');
-        resultadoF.innerHTML = FuncaoFarenheitNumber;
+        resultadoF.innerHTML = FuncaoFarenheitNumber + "°F";
+    }
+}
+
+    function ConversaoFarenheitParaCelsius() {
+    var entradaFarenheit = document.getElementById('valorFarenheit');
+    var entradaFarenheitNumero = parseFloat(entradaFarenheit.value);
+
+    if (entradaFarenheitNumero != 0) {
+        let FuncaoCelsius = ( entradaFarenheitNumero - 32 ) * ( 5 / 9 );
+        let FuncaoCelsiusNumber = parseFloat(FuncaoCelsius.toFixed(2));
+        let resultadoC = document.getElementById('resultadoCelsius');
+        resultadoC.innerHTML = FuncaoCelsiusNumber + "°C";
     }
 }
